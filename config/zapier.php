7ac5c8852dca3ee430375b9530b799843a1c7df9
Benjamin Heller <benjaminheller@pop-os.localdomain>
2024-01-03 17:16:12 -0900

    This one too

--- config/zapier.php
+++ config/zapier.php
@@ -11,7 +11,7 @@ return [
     'zaps' => [
         'url' => env('ZAPIER_ZAPS_URL', ''),
         'group-id' => env('ZAPIER_ZAPS_GROUP_ID', ''),
-        'hooks' => env('ZAPIER_HOOKS', []),
+        'hooks' => env('ZAPIER_HOOKS', ''),
         'global-data' => [
             'querystring' => [
                 // querystring global params for all hooks
